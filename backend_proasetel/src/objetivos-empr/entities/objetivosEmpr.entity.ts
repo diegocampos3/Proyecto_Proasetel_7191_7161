c18ea6fb0614f1468390d62775189f0de99eb4e2
Diego Campos <camposdiegog3@gmail.com>
2024-12-09 23:34:32 -0500

    Validación de Objetivos Empresariales

--- backend_proasetel/src/objetivos-empr/entities/objetivosEmpr.entity.ts
+++ backend_proasetel/src/objetivos-empr/entities/objetivosEmpr.entity.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class ObjetivosEmpr {
