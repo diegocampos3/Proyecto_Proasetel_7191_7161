670931e0778c8ad8dd8529d5270b604f10694630
Diego Campos <camposdiegog3@gmail.com>
2024-12-24 20:20:11 -0500

    Gestión de Periodos, Gestión de Perido_Evaluación, Validaciones

--- backend_proasetel/src/periodo-evaluacion/periodo-evaluacion.service.ts
+++ backend_proasetel/src/periodo-evaluacion/periodo-evaluacion.service.ts
@@ -1,26 +1,144 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreatePeriodoEvaluacionDto } from './dto/create-periodo-evaluacion.dto';
 import { UpdatePeriodoEvaluacionDto } from './dto/update-periodo-evaluacion.dto';
+import { PeriodoEvaluacion } from 'src/data-access/entities/periodoEvaluacion.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/data-access/entities/usuario.entity';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class PeriodoEvaluacionService {
-  create(createPeriodoEvaluacionDto: CreatePeriodoEvaluacionDto) {
-    return 'This action adds a new periodoEvaluacion';
+  
+  private readonly logger = new Logger('Periodo');
+
+  constructor(
+
+   @InjectRepository(PeriodoEvaluacion)
+    private readonly periodoEvaRepository: Repository<PeriodoEvaluacion>
+  ){}
+  
+  
+  
+  async create(createPeriodoEvaluacionDto: CreatePeriodoEvaluacionDto) {
+    try {
+      const { idPeriodo, idUser } = createPeriodoEvaluacionDto;
+  
+      if (!isUUID(idPeriodo)) {
+        throw new BadRequestException('El periodo ingresado no existe o no es válido');
+      }
+  
+      if (!isUUID(idUser)) {
+        throw new BadRequestException('El usuario ingresado no existe o no es válido');
+      }
+  
+      const periodoEva = this.periodoEvaRepository.create({
+        periodo: { idPeriodo }, 
+        user: { id: idUser } as User,      
+      });
+  
+      await this.periodoEvaRepository.save(periodoEva);
+  
+      return periodoEva;
+
+    } catch (error) {
+
+      console.error('Error al crear la evaluación de periodo:', error.message);
+      throw new InternalServerErrorException('Error al crear la evaluación de periodo');
+    }
+  }
+  
+  
+  async findAll() {
+    try {
+      const periodoEvaluaciones = await this.periodoEvaRepository.find({
+        relations: ['periodo', 'user'], 
+      });
+  
+      if (!periodoEvaluaciones || periodoEvaluaciones.length === 0) {
+        throw new BadRequestException('No se encontraron evaluaciones de periodo');
+      }
+  
+      return periodoEvaluaciones;
+    } catch (error) {
+      throw new InternalServerErrorException('Error al obtener las evaluaciones de periodo');
+    }
+  }
+  
+  
+
+  async findOne(id: string) {
+    try {
+      if (!isUUID(id)) {
+        throw new BadRequestException('El ID de periodo no es válido');
+      }
+  
+      const periodoEva = await this.periodoEvaRepository.findOne({
+        where: { idPeriodoEva: id },
+        relations: ['periodo', 'user'], 
+      });
+  
+      if (!periodoEva) {
+        throw new BadRequestException('No se encontró la evaluación de periodo con ese ID');
+      }
+  
+      return periodoEva;
+    } catch (error) {
+      throw new InternalServerErrorException('Error al obtener la evaluación de periodo');
+    }
+  }
+  
+  async update(id: string, updatePeriodoEvaluacionDto: UpdatePeriodoEvaluacionDto) {
+    try {
+      if (!isUUID(id)) {
+        throw new BadRequestException('El ID de periodo no es válido');
+      }
+  
+      const periodoEva = await this.periodoEvaRepository.findOne({ where: { idPeriodoEva: id } });
+      if (!periodoEva) {
+        throw new BadRequestException('No se encontró la evaluación de periodo con ese ID');
+      }
+  
+      const updatedPeriodoEva = Object.assign(periodoEva, updatePeriodoEvaluacionDto);
+      await this.periodoEvaRepository.save(updatedPeriodoEva);
+  
+      return updatedPeriodoEva;
+    } catch (error) {
+      throw new InternalServerErrorException('Error al actualizar la evaluación de periodo');
+    }
+  }
+  
+
+  async remove(id: string) {
+    try {
+      if (!isUUID(id)) {
+        throw new BadRequestException('El ID de periodo no es válido');
+      }
+  
+      const periodoEva = await this.periodoEvaRepository.findOne({ where: { idPeriodoEva: id } });
+      if (!periodoEva) {
+        throw new BadRequestException('No se encontró la evaluación de periodo con ese ID');
+      }
+  
+      await this.periodoEvaRepository.remove(periodoEva);
+  
+      return { message: 'Evaluación de periodo eliminada exitosamente' };
+    } catch (error) {
+      throw new InternalServerErrorException('Error al eliminar la evaluación de periodo');
+    }
+  }
+  
+
+  private handleDBExceptions( error: any): never{
+    if( error.code === '23505')
+      throw new BadRequestException(error.detail)
+
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 
-  findAll() {
-    return `This action returns all periodoEvaluacion`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} periodoEvaluacion`;
-  }
-
-  update(id: number, updatePeriodoEvaluacionDto: UpdatePeriodoEvaluacionDto) {
-    return `This action updates a #${id} periodoEvaluacion`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} periodoEvaluacion`;
-  }
+  
 }
+
+
